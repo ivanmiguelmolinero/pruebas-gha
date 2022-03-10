4f9e91073ec531fcf25485004e23cb28fc3edc30
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-03-10 13:36:49 +0100

    Segunda prueba sonar

--- test_class.py
+++ test_class.py
@@ -1,6 +1,7 @@
 from calc import calculadora
 
+
 def test_class_monkey(monkeypatch):
     monkeypatch.setattr(calculadora, "sumar", lambda x: 4)
-    c = calculadora()
-    assert c.sumar() == 4
+    cal = calculadora()
+    assert cal.sumar() == 4
